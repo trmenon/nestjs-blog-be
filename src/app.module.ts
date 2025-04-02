58f43c031f4a7710b8e30c70038921a1836cb879
Tushar Menon <trmenon@gmail.com>
2025-04-03 03:50:53 +0530

    #feat: implemented configuration for typeorm integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,42 @@ import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
 
+// TypeORM
+import {TypeOrmModule} from '@nestjs/typeorm';
+
 
 @Module({
   imports: [    
-  UsersModule, 
-  PostsModule, AuthModule
+    UsersModule, 
+    PostsModule, 
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: ()=> ({
+        type: 'postgres', // Type of Database connecting to
+        entities: [], // Entities based on which ORM will create tables in the database
+        /**
+         * synchronize must be used only in development mode
+         * Creates DB schemas in your application
+         * This could be destructive in production mode
+         */
+        synchronize: true,
+        /**
+         * Postgress sql related configurations
+         * port: Port number <default is 5432>
+         * username: Username of the postgres server
+         * password: Password of the associated username
+         * host: host <localhost for development>
+         * database: Database identification name
+         */
+        port: 5432,
+        username: 'postgres',
+        password: 'password',
+        host: 'localhost',
+        database: 'nestjs-blog'
+      })      
+    })
 ],
   controllers: [AppController],
   providers: [AppService],
